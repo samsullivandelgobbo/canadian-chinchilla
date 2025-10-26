5b3bf90b7a44d3ca5169f492e7b51f56c3015799
samsullivandelgobbo <sam.sullivan150@gmail.com>
2025-10-25 21:50:02 -0400

    vercel speed insights & analytics

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import Banner from "@/components/banner";
@@ -99,6 +101,8 @@ export default function MainLayout({
         <Banner />
         <main className="min-h-screen texture-overlay">{children}</main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
